c9ce572ce1df459188ecc63073cf7451bf617167
Raimon Esteve <raimon@nan-tic.com>
2018-04-19 14:25:20 +0200

    Add external discount field

--- sale.py
+++ sale.py
@@ -7,21 +7,24 @@ from trytond.pyson import Eval
 from trytond.config import config as config_
 
 __all__ = ['Sale']
-__metaclass__ = PoolMeta
 
 DIGITS = config_.getint('product', 'price_decimal', default=4)
+DISCOUNT_DIGITS = config_.getint('product', 'discount_decimal', default=4)
 
 
 class Sale:
+    __metaclass__ = PoolMeta
     __name__ = 'sale.sale'
-    external_untaxed_amount = fields.Numeric('External Untaxed', readonly=True,
-        digits=(16, DIGITS))
-    external_tax_amount = fields.Numeric('External Tax', readonly=True,
-        digits=(16, DIGITS))
-    external_total_amount = fields.Numeric('External Total', readonly=True,
-        digits=(16, DIGITS))
-    external_shipment_amount = fields.Numeric('External Total Shipment', readonly=True,
-        digits=(16, DIGITS))
+    external_untaxed_amount = fields.Numeric('External Untaxed',
+        readonly=True, digits=(16, DIGITS))
+    external_tax_amount = fields.Numeric('External Tax',
+        readonly=True, digits=(16, DIGITS))
+    external_total_amount = fields.Numeric('External Total',
+        readonly=True, digits=(16, DIGITS))
+    external_shipment_amount = fields.Numeric('External Total Shipment',
+        readonly=True, digits=(16, DIGITS))
+    external_discount = fields.Numeric('External Discount',
+        readonly=True, digits=(16, DISCOUNT_DIGITS))
 
     @classmethod
     def view_attributes(cls):
